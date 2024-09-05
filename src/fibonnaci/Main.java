80f0b3d9cd62ad8f0640b74dda5ea55a9980a86d
matheusob25 <matheusoliveira0890@gmail.com>
2024-09-04 22:22:48 -0300

    ocorrencias de string

--- src/fibonnaci/Main.java
+++ src/fibonnaci/Main.java
@@ -2,13 +2,14 @@ package fibonnaci;
 
 public class Main {
 
-    //parâmetros 'n' para saber até que número a soma de fibonnaci vai, e 'comparacao' vai ser o número ...
-    // que será comparado com todos os números gerados na soma
+    //parâmetros 'n' para saber até que número a soma de fibonnaci vai
     public static int fibonnaci(int n){
+        //ponto onde acaba o loop que a recursividade gera
         if(n == 0 || n == 1){
             return n;
         }
-
+    //aplicando recursividade onde eu somo n - 1 que será o primeiro valor no looping e n - 2 o segundo,
+    //e após isso de forma empilhada as funções vão somando até chegar ao ponto de parar (condição para parar a função 'if(n == 0 || n == 1)')
         return fibonnaci(n-1) + fibonnaci(n-2);
     }
 
--- src/ocorrenciasString/ocorrenciaString.java
+++ src/ocorrenciasString/ocorrenciaString.java
@@ -1,4 +1,25 @@
 package ocorrenciasString;
 
 public class ocorrenciaString {
+    public static void verificaAparicaoString(String e, char letraProcurada){
+        int quantidadeDeOcorrencias = 0;
+        // string auxiliar para utilizar o toLowerCase
+        String aux = String.valueOf(letraProcurada);
+
+        for (int i = 0; i < e.length(); i++) {
+            //comparando o char em letra minuscula com cada caracter da string também em letra minúscula
+            if(aux.toLowerCase().charAt(0) == e.toLowerCase().charAt(i) ){
+                System.out.println("Letras encontrada: " + e.charAt(i));
+                //  somatório de ocorrências da letra
+                quantidadeDeOcorrencias++;
+            }
+
+        }
+        System.out.println("Quantidade de ocorrencias da letra " + letraProcurada + ": " + quantidadeDeOcorrencias);
+    }
+    public static void main(String[] args) {
+        verificaAparicaoString("Arroz com feijao e ovo",'a');
+
+
+    }
 }
